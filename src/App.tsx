06c966998945b4bcbdff56e1ce73f871559f6b09
AlanF611 <72899298+AlanF611@users.noreply.github.com>
2025-11-09 15:05:50 -0600

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import ScrollToTop from './components/ScrollToTop'; // Importa el componente
 import HomePage from './pages/HomePage';
 import ServicesPage from './pages/ServicesPage';
 import PortfolioPage from './pages/PortfolioPage';
@@ -11,6 +12,7 @@ function App() {
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
+      <ScrollToTop /> {/* Agrega esto aquí */}
       <main className="flex-grow">
         <Routes>
           <Route path="/" element={<HomePage />} />
